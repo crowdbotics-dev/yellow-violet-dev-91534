4bb802bd6866f8625d568a236a4d8507c68b1175
Crowdbotics <engineering@crowdbotics.com>
2023-06-14 13:23:07 +0000

    Initial commit.

---
+++ backend/yellow_violet_dev_91534/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for yellow_violet_dev_91534 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yellow_violet_dev_91534.settings')
+
+application = get_wsgi_application()
